10f0e35e746f43d553b2cc5ce0e7c59751a00690
Divyam Sirswal <119831005+Divyamsirswal@users.noreply.github.com>
2024-07-22 10:28:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,9 @@ function App() {
 
   return (
     <AnimatePresence mode="wait">
+      <BrowserRouter>
       <Routes location={location} key={location.pathname}>
+        
         <Route path="/" element={<HomePage />} />
         <Route path="/events" element={<Events />} />
         <Route path="/events/upcoming" element={<Upcoming />} />
@@ -40,6 +42,7 @@ function App() {
         <Route path="/resources" element={<ResourcesPage />} />
         <Route path="/contactus" element={<ContactusPage />} />
       </Routes>
+        </BrowserRouter>
     </AnimatePresence>
   );
 }
